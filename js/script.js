6092253a6e2f59125cec5359bca40ea342976247
Adam Rajba <adamrajba@gmail.com>
2020-11-20 21:16:13 +0000

    Remove active class js

--- js/script.js
+++ js/script.js
@@ -4,25 +4,35 @@
     console.log('links:', links);
   });  */
   
-  const titleClickHandler = function(event){
+const titleClickHandler = function(event){
     console.log('Link was clicked!');
     console.log('Events:', event)
   
-    /* remove class 'active' from all article links  */
+/* remove class 'active' from all article links  */
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
   
-    /* add class 'active' to the clicked link */
+ /* add class 'active' to the clicked link */
+ const activeArticles = document.querySelectorAll('.posts a.active');
+
+ for(let activeArticle of activeArticles){
+   activeArticle.classList.remove('active');
+ }
   
-    /* remove class 'active' from all articles */
+/* remove class 'active' from all articles */
   
-    /* get 'href' attribute from the clicked link */
+/* get 'href' attribute from the clicked link */
   
-    /* find the correct article using the selector (value of 'href' attribute) */
+/* find the correct article using the selector (value of 'href' attribute) */
   
-    /* add class 'active' to the correct article */
-  }
+/* add class 'active' to the correct article */
+}
   
-  const links = document.querySelectorAll('.titles a');
+const links = document.querySelectorAll('.titles a');
   
-  for(let link of links){
+for(let link of links){
     link.addEventListener('click', titleClickHandler);
-  }
+}
